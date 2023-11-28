2f35701b79445946ab4893a2d8334a7e2911c6f4
Demuser1 <150330144+Demuser1@users.noreply.github.com>
2023-11-28 16:27:38 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Cats")
+option_a = os.getenv('OPTION_A', "Catsss")
 option_b = os.getenv('OPTION_B', "Dogs")
-print("this change is commited with CI/CD Pipeline")
 hostname = socket.gethostname()
 
 app = Flask(__name__)

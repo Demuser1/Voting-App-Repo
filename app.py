66511a6d2d653daeb8f13e80418ca4148dc975ec
Demuser1 <hardcracker007@hotmail.com>
2023-11-29 13:19:23 +0500

    2nd commit

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "Cats333")
-option_b = os.getenv('OPTION_B', "Dogs333")
+option_a = os.getenv('OPTION_A', "Cats777")
+option_b = os.getenv('OPTION_B', "Dogs8888")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
